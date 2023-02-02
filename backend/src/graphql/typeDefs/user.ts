92764e83766645294ee7ceac39cd04257e173911
“Mert-digi” <mert@digiterra.com.tr>
2023-02-03 00:09:00 +0300

    aesthecticinturkey#7 Init backend

---
+++ backend/src/graphql/typeDefs/user.ts
@@ -0,0 +1,26 @@
+import { gql } from "apollo-server-core";
+
+const typeDefs = gql`
+    type User {
+        id: String
+        username: String
+
+    }
+
+    type Query {
+        searchUsers(username: String): [User]
+    }
+
+    type Mutanion {
+        createUsername(username: String): CreateUsernameResponse
+    }
+
+    type CreateUsernameResponse {
+        success: Boolean
+        error: String
+    }
+`;
+
+export default typeDefs
+
+
