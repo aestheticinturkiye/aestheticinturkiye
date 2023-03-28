2e99a023c319e3f98aea42731b9b8f314cb8cc75
Mert Yuşa Atağ <69805617+mertyusaatag@users.noreply.github.com>
2023-03-29 00:41:31 +0300

    Delete src/main directory

--- src/main/java/com/esthomy/version1/repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esthomy.version1.repository;
-
-import com.esthomy.version1.model.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital,String> {
-}
