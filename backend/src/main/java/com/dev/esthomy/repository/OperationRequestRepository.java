a488c70db322e66b115bda02f07c74f226ab79e5
m00836492 <mert.yusa.atag@huawei.com>
2023-08-17 16:29:12 +0300

    imrpvnts

--- backend/src/main/java/com/dev/esthomy/repository/OperationRequestRepository.java
+++ backend/src/main/java/com/dev/esthomy/repository/OperationRequestRepository.java
@@ -3,5 +3,8 @@ package com.dev.esthomy.repository;
 import com.dev.esthomy.models.OperationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OperationRequestRepository extends JpaRepository<OperationRequest, String> {
+    List<OperationRequest> findAllByUserAccountId(String accountId);
 }
