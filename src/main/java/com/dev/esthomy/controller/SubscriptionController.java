115fa0870d9b7b5060ca8df8ccdcf7c2f7d04e67
MustafaColakoglu <mstfclkglu@gmail.com>
2024-07-28 21:36:45 +0300

    Response entity

--- src/main/java/com/dev/esthomy/controller/SubscriptionController.java
+++ src/main/java/com/dev/esthomy/controller/SubscriptionController.java
@@ -7,6 +7,7 @@ import com.dev.esthomy.dto.response.UpdateSubscriptionResponse;
 import com.dev.esthomy.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class SubscriptionController {
     @PostMapping
     public ResponseEntity<CreateSubscriptionResponse> create(@RequestBody final CreateSubscriptionRequest request) {
         log.info(request.toString());
-        return ResponseEntity.ok().body(subscriptionService.create(request));
+        return new ResponseEntity<>(subscriptionService.create(request), HttpStatus.OK);
     }
 
     @PutMapping("{id}")
